323e8bb382f82ccf353ad88ff80b6cada4799dc5
Sanjaya1822 <san9345420@gmail.com>
2026-05-10 16:27:02 +0530

    Create Leetcode-48.java

---
+++ Leetcode-48.java
@@ -0,0 +1,33 @@
+Problem: Rotate Image
+
+Leetcode Link : https://leetcode.com/problems/rotate-image/description/
+
+Solution:
+
+class Solution {
+    public int calPoints(String[] operations) {
+        List<Integer> arr=new ArrayList<>();
+
+        for(int i=0;i<operations.length;i++){
+            String ch= operations[i];
+            if(ch.equals("C")){
+                arr.remove(arr.size()-1);
+            }
+            else if(ch.equals("D")){
+                int x= arr.get(arr.size()-1)*2;
+                arr.add(x);
+            }
+            else if(ch.equals("+")){
+                int x= arr.get(arr.size()-1) + arr.get(arr.size()-2);
+                arr.add(x);
+            }else {
+                arr.add(Integer.valueOf(ch));
+            }
+        }   
+        int sum=0;
+        for(int num: arr){
+            sum+=num;
+        }
+        return sum;
+    }
+}
